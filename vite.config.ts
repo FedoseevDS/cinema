8d6d2180d3e66b845c8d0e69d7866cc35182d7f9
FedoseevDS <dmsr.fedoseev@gmail.com>
2024-09-16 12:54:45 +0300

    Подготовил проект к работе

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5004,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
 })
