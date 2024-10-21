9b2f622df9d581258c1b33cd3dbcd2239512b94b
FedoseevDS <dmsr.fedoseev@gmail.com>
2024-10-21 10:38:39 +0300

    Поправил сохранение истории

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -6,15 +6,19 @@ type SelectProps = {
   label: string;
   options: { name: string; value: string }[];
   defaultValue: boolean;
+  valueUrl: string;
   onChange: (e: any) => any;
 };
 
-const Select = ({ defaultValue, label, options, onChange }: SelectProps) => {
+const Select = ({ defaultValue, label, options, valueUrl, onChange }: SelectProps) => {
   return (
     <div className={styles.wrapper}>
       <label>{label}</label>
       <div>
-        <select onChange={({ target }) => onChange({ label, value: target.value })}>
+        <select
+          onChange={({ target }) => onChange({ label, value: target.value })}
+          value={valueUrl}
+        >
           {options?.map(({ name, value }, index) => (
             <option key={name + value} value={value} selected={!defaultValue && index === 0}>
               {name}
